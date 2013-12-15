800389d3f01249760746a864246bd2e8b7efc080
Al Newkirk <awncorp@cpan.org>
2013-12-15 18:46:38 +0000

    Import of AWNCORP/Bubblegum-0.02 from CPAN.
    gitpan-cpan-distribution: Bubblegum
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AWNCORP/Bubblegum-0.02.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

--- lib/Bubblegum.pm
+++ lib/Bubblegum.pm
@@ -5,7 +5,7 @@ use Moo;
 
 with 'Bubblegum::Role::Configuration';
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 sub import {
     my $target = caller;
@@ -30,7 +30,7 @@ Bubblegum - Opinionated Modern Perl Development Framework
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head2 Introduction
 
